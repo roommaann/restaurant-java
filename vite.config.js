5ae8e505daf7374875030947d9a6ef9e9630877a
roommaann <155384702+roommaann@users.noreply.github.com>
2025-01-02 14:03:32 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Web-Tech-Project/', // Add your repository name here
   plugins: [react()],
 })
